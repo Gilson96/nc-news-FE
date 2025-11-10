2c6a1cdba3bc545ca54e35b8924a93ac0b87b346
Gilson96 <grafael99@gmail.com>
2025-11-10 14:30:43 +0000

    Added Navigator and Home components to main file

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router";
 import "./App.css";
 import Home from "./components/Home/home";
+import Navigator from "./components/Navigator/navigator";
 
 function App() {
   return (
     <>
+      <Navigator />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
