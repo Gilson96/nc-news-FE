2112a0260464787b9db009e028375bf34967ad42
Gilson96 <grafael99@gmail.com>
2025-11-11 15:09:28 +0000

    Added data types for comments response

--- src/utils/dataTypes.ts
+++ src/utils/dataTypes.ts
@@ -11,9 +11,32 @@ export interface Article {
 
 export type Articles = Article[];
 
+export type ArticleObj = {
+    article: Article
+}
+
+export interface User {
+    username: string,
+    name: string,
+    avatar_url: string
+}
+
+export type Users = User[]
+
 export interface Topic {
     slug: string,
     description: string
 }
 
 export type Topics = Topic[];
+
+export interface Comment {
+    comment_id: number,
+    article_id: number,
+    body: string,
+    votes: number,
+    author: string,
+    created_at: string
+}
+
+export type CommentsArray = Comment[];
