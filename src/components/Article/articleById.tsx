41ebffe76c2c7ab7cf537df1c3d3d1aef0dc8c16
Gilson96 <grafael99@gmail.com>
2025-11-11 15:09:52 +0000

    Refined UI

--- src/components/Article/articleById.tsx
+++ src/components/Article/articleById.tsx
@@ -1,5 +1,102 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import type { ArticleObj, Users } from "../../utils/dataTypes";
+import { dateOnlyFormat } from "../../utils/timeFormat";
+import { useParams } from "react-router";
+import Comments from "./comments";
+import InfoButtons from "../ui/infoButtons";
+
 const ArticleById = () => {
-  return <div>ArticleById</div>;
+  const [article, setArticle] = useState<ArticleObj>();
+  const [user, setUser] = useState<Users>();
+  const [isArticleLoading, setIsArticleLoading] = useState(true);
+  const [isUserLoading, setIsUserLoading] = useState(true);
+  const articleId = useParams();
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://nc-news-api-99f5fdc34977.herokuapp.com/api/articles/${Number(articleId.article_id)}`,
+      )
+      .then(function (response) {
+        setArticle(response.data);
+        setIsArticleLoading(false);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    axios
+      .get(`https://nc-news-api-99f5fdc34977.herokuapp.com/api/users`)
+      .then(function (response) {
+        setUser(response.data);
+        setIsUserLoading(false);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
+
+  const findUserImage = user?.find(
+    (u) => u.username === article?.article.author,
+  )?.avatar_url;
+
+  return (
+    <article className="flex w-full flex-col px-[3%] pt-[4%]">
+      <div className="flex items-center gap-1">
+        {isUserLoading ? (
+          <span
+            className={`animate h-8 w-8 animate-pulse rounded-full bg-gray-300`}
+          ></span>
+        ) : (
+          <img
+            className={`h-8 w-8 rounded-full`}
+            src={findUserImage}
+            alt="user avatar"
+          />
+        )}
+        <p
+          className={`${isArticleLoading && "animate h-4 w-12 animate-pulse bg-gray-300"}`}
+        >
+          {isArticleLoading ? "" : article?.article.author}
+        </p>
+        <span>&#183;</span>
+        <p
+          className={`${isArticleLoading && "animate h-4 w-12 animate-pulse bg-gray-300"}`}
+        >
+          {isArticleLoading ? "" : dateOnlyFormat(article?.article.created_at!)}
+        </p>
+      </div>
+
+      <div className="flex flex-col gap-2 pt-[3%]">
+        <p
+          className={`${isArticleLoading ? "animate h-4 w-20 animate-pulse bg-gray-300" : "font-bold"}`}
+        >
+          {isArticleLoading ? "" : article?.article.title}
+        </p>
+        {isArticleLoading ? (
+          <span
+            className={`animate h-40 w-52 animate-pulse rounded-xl bg-gray-300`}
+          ></span>
+        ) : (
+          <img
+            className={`h-[60] w-full rounded-xl`}
+            src={article?.article.article_img_url}
+            alt="user avatar"
+          />
+        )}
+
+        <div className="flex w-full items-center justify-start gap-3 border-b py-[2%] text-sm">
+          <InfoButtons
+            author={article === undefined ? "n/a" : article?.article.author}
+            count={article === undefined ? 0 : article?.article.count}
+            votes={article === undefined ? 0 : article?.article.votes}
+          />
+        </div>
+
+        <Comments articleId={Number(articleId.article_id)} />
+      </div>
+    </article>
+  );
 };
 
 export default ArticleById;
--- src/components/Article/articles.tsx
+++ src/components/Article/articles.tsx
@@ -2,9 +2,10 @@ import { useEffect, useState } from "react";
 import type { Articles } from "../../utils/dataTypes";
 import axios from "axios";
 import { dateOnlyFormat } from "../../utils/timeFormat";
-import { ArrowBigUpDash, MessageCircle, User2 } from "lucide-react";
+import { Loader2 } from "lucide-react";
 import type { HomeProps } from "../Home/home";
 import { Link } from "react-router";
+import InfoButtons from "../ui/infoButtons";
 
 const Article = ({ sort_by, topic, order }: HomeProps) => {
   const [articles, setArticles] = useState<Articles>();
@@ -27,47 +28,7 @@ const Article = ({ sort_by, topic, order }: HomeProps) => {
   return (
     <main className="flex w-full flex-col p-[2%]">
       {isLoading ? (
-        <article className="flex w-full flex-col border-b p-[2%]">
-          <div className="flex w-full items-start justify-between">
-            <div className="w-[30%]">
-              <div className="animate h-24 w-24 animate-pulse rounded bg-gray-300"></div>
-            </div>
-            <div className="flex w-full flex-col items-end gap-2 text-right">
-              <h1 className="animate animate-pulse bg-gray-300 text-sm text-gray-300">
-                title
-              </h1>
-              <p className="animate animate-pulse bg-gray-300 text-sm text-gray-300">
-                date
-              </p>
-              <div className="flex w-full items-center justify-end gap-3 text-sm">
-                <p className="flex w-auto items-center justify-center gap-1 rounded-full border p-[2%]">
-                  <span>
-                    <User2 size={15} />
-                  </span>
-                  <span className="animate animate-pulse bg-gray-300 text-sm text-gray-300">
-                    | author
-                  </span>
-                </p>
-                <p className="flex items-center justify-center gap-1 rounded-full border px-[4%] py-[2%]">
-                  <span>
-                    <ArrowBigUpDash size={15} />
-                  </span>
-                  <span className="animate animate-pulse bg-gray-300 text-sm text-gray-300">
-                    | votes
-                  </span>
-                </p>
-                <p className="flex items-center justify-center gap-1 rounded-full border p-[2%]">
-                  <span>
-                    <MessageCircle size={15} />
-                  </span>
-                  <span className="animate animate-pulse bg-gray-300 text-sm text-gray-300">
-                    | count
-                  </span>
-                </p>
-              </div>
-            </div>
-          </div>
-        </article>
+        <Loader2 className="animate animate-spin" />
       ) : (
         articles?.map((article) => (
           <article className="flex w-full flex-col border-b p-[2%]">
@@ -85,24 +46,11 @@ const Article = ({ sort_by, topic, order }: HomeProps) => {
                     {dateOnlyFormat(article.created_at)}
                   </p>
                   <div className="flex w-full items-center justify-end gap-3 text-sm">
-                    <p className="flex w-auto items-center justify-center gap-1 rounded-full border p-[2%]">
-                      <span>
-                        <User2 size={15} />
-                      </span>
-                      <span>| {article.author}</span>
-                    </p>
-                    <p className="flex items-center justify-center gap-1 rounded-full border px-[4%] py-[2%]">
-                      <span>
-                        <ArrowBigUpDash size={15} />
-                      </span>
-                      <span>| {article.votes}</span>
-                    </p>
-                    <p className="flex items-center justify-center gap-1 rounded-full border p-[2%]">
-                      <span>
-                        <MessageCircle size={15} />
-                      </span>
-                      <span>| {article.count}</span>
-                    </p>
+                    <InfoButtons
+                      author={article.author}
+                      count={article.count}
+                      votes={article.votes}
+                    />
                   </div>
                 </div>
               </div>
