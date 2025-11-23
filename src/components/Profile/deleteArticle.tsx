e707d292293243d7e8c8567f1e6828782a36d669
Gilson96 <grafael99@gmail.com>
2025-11-23 04:06:09 +0000

    Add delete an article functionality

--- src/components/Profile/deleteArticle.tsx
+++ src/components/Profile/deleteArticle.tsx
@@ -1,4 +1,4 @@
-import axios from "axios";
+import { useDeleteArticle } from "../../hooks/useDeleteActions";
 import {
   AlertDialog,
   AlertDialogAction,
@@ -10,42 +10,14 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from "../ui/alert-dialog";
-import { useGetTopics } from "../../hooks/useFetchActions";
-import { useEffect, useState } from "react";
 
 const DeleteArticle = ({ article_id }: { article_id: number }) => {
-  const [successDelete, setSuccessDelete] = useState(false);
-  const { topics, isLoading } = useGetTopics();
-
-  const handleDeleteArticle = () => {
-    axios
-      .delete(
-        `https://nc-news-api-99f5fdc34977.herokuapp.com/api/articles/${article_id}`,
-      )
-      .then((response) => {
-        setSuccessDelete(true);
-      })
-      .catch((err) => console.log(err));
-  };
-
-  useEffect(() => {
-    const emptyTopic = topics?.find((topic) => topic.count === "0")?.slug;
-    if (emptyTopic !== undefined) {
-      axios
-        .delete(
-          `https://nc-news-api-99f5fdc34977.herokuapp.com/api/topics/${emptyTopic}`,
-        )
-        .then((response) => {
-          console.log(response);
-        })
-        .catch((err) => console.log(err));
-    }
-  }, [topics]);
+  const { handleDeleteArticle } = useDeleteArticle(article_id);
 
   return (
     <AlertDialog>
       <AlertDialogTrigger className="cursor-pointer">
-        <button className="h-[2rem] w-[4rem] rounded bg-red-500 p-[2%] text-white">
+        <button className="h-8 w-16 cursor-pointer rounded bg-red-500 p-[2%] text-white">
           Delete
         </button>
       </AlertDialogTrigger>
